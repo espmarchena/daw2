bd3ec484cb66994e4c139ccfe451f0e1fb4fc001
espmarchena <espemarchena26@gmail.com>
2025-09-29 08:33:26 +0200

    actualizacion 26/09

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $usuariodb = 'emm';
 $clavedb = 'Palomares@1011';
 $opcionesDB = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //modo de error
-    PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC, //modo de fetch
+    PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC, //modo de fetch que significa que devuelve un array asociativo
 ];
 
 //PDO es la clase por la cual se hace la conexion a la base de datos
--- productos.php
+++ productos.php
@@ -9,14 +9,14 @@ $stmt = $conexion->prepare($sql); //prepara la consulta
 $stmt->execute(); //ejecuta la consulta
 
 $listado='';
-while ($reg = $stmt->fetch()) { //recorre los resultados de la consulta
-    $listado .= '<li>'.$reg['producto'].': '.$reg['precio'].'</li>'; //almacena los productos en una lista
+while ($registro = $stmt->fetch()) { //recorre los resultados de la consulta (un fetch coge los datos brutos y los prepara en forma de matriz)
+    $listado .= '<li>'.$registro['producto'].': '.$registro['precio'].'</li>'; //almacena los productos en una lista
 }
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
